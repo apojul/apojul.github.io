1d034a39c92a98a2ce801f9e61eab43aecf04050
Andres Gomez <andres@lilo.org>
2020-10-16 14:00:20 +0200

    added state variables

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -6,7 +6,25 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    // These objects are the content of the application
+  boards: undefined,
+  tasks: undefined,
+  lists: undefined,
+
+  activeBoard: undefined,
+  activeList: undefined,
+  activeTask: undefined,
+  // Dictionaires de liason à utiliser avec un getter 
+  boardsList: undefined, // cest juste une liste d'id.
+  listList: undefined,
+
+  // These objects concern the user and users information
+  activeUser: undefined, //current user
+  Users: undefined, //all the other users
+  OnLineUsers: undefined // other users who are currently online
+
+  },
   mutations: {},
   actions: {},
   modules: {}
