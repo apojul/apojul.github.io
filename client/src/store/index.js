529e84b964ebf98fd903d32279e04c570985ac6c
Andres Gomez <andres@lilo.org>
2020-10-16 14:09:27 +0200

    added store actions and mutations

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -25,7 +25,80 @@ export default new Vuex.Store({
   OnLineUsers: undefined // other users who are currently online
 
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    SET_BOARDS: (state, boardList) => {
+      state.boards = {};
+      boardList.forEach(board => {
+        state.boards[board.id] = board;
+      });
+    },
+    SET_BOARD: (state, board) => {
+      //is equal to state.boards[board.id]=board
+      Vue.set(state.boards, board.id, board);
+    },
+    // Lists manipulation
+    SET_LISTS: (state, listList) => {
+      state.lists = {};
+      listList.forEach(list => {
+        state.lists[list.id] = list;
+      });
+    },
+    // User manipulation
+    SET_USER: (state, user) => {
+      state.activeUser = user;
+    },
+    CLEAR_SESSION: state => {
+      state.activeUser = undefined;
+    },
+    SET_CON_USER_LIST: (state, conUsers) => {
+      state.OnLineUsers = conUsers.authenticatedUsers.map(user => user.id)
+    },
+    // SET_OTHER_USERS: (state, user) => {
+    //   state.otherUsers = {};
+    //   user.forEach(user => {
+    //     state.otherUsers[user.id] = user;  
+    //   }).filter(user => user.id !== "9") //s'enlever soi meme
+    // console.log("filtered users ", state.otherUsers) 
+    // }
+  },
+  actions: {
+    // Board related actions
+  async FETCH_BOARD_LIST({ commit }) {
+    let boardList = await app.service("boards").find();
+    commit("SET_BOARDS", boardList);
+  },
+  async CREATE_BOARD(_, payload) {
+    console.log("payload: ", payload);
+    let newBoard = await app.service("boards").create({
+      ...payload
+    });
+    console.log("newBoard =", newBoard);
+  },
+  // USER related actions
+  async AUTHENTICATE_USER(_, payload) {
+    try {
+      return await app.reAuthenticate();
+    } catch (error) {
+      return await app.authenticate({
+        strategy: "local",
+        ...payload
+      });
+    }
+  },
+  async LOG_OUT() {
+    await app.logout();
+  }, 
+  async REGISTER_USER(_, payload) {
+    console.log("payload: ", payload);
+    await app.service("users").create({
+      ...payload
+    });
+  },
+  async FETCH_OTHER_USERS ({commit}) {
+    let othetUsersList = await app.service("users").find();
+    commit("SET_OTHER_USERS", othetUsersList);
+  }, 
+ 
+  },
   modules: {}
 });
