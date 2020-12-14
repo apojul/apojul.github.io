4d9958b20d49f3fe0b33222bad0987e7b041cf42
Andres Gomez <andres@lilo.org>
2020-12-14 13:48:52 +0100

    knexfile.js clg de connection et app_server

--- server/src/knex.js
+++ server/src/knex.js
@@ -2,8 +2,13 @@ const knex = require('knex')
 
 module.exports = function (app) {
   const { client, connection } = app.get('postgres')
+
   const db = knex({ client, connection })
   //test system env variable $APP_DB
   console.log('connection db in knex.js', connection);
+  // test sytem env variable APP_SERVER
+  const {oauth} = app.get('authetication')
+  console.log('oauth in kenx.js', oauth);
+
   app.set('knexClient', db)
 }
