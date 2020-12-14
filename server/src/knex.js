4d75e89180c450db8b59ff576791843e607c1e6e
Andres Gomez <andres@lilo.org>
2020-12-14 13:35:55 +0100

    knexfile.js clg de connection

--- server/src/knex.js
+++ server/src/knex.js
@@ -3,6 +3,7 @@ const knex = require('knex')
 module.exports = function (app) {
   const { client, connection } = app.get('postgres')
   const db = knex({ client, connection })
-
+  //test system env variable $APP_DB
+  console.log('connection db in knex.js', connection);
   app.set('knexClient', db)
 }
