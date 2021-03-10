e2d54a1fec02a5242da347f0909b6206dafc6c28
Pascal ALLAU <pascal.allau@etu.inp-n7.fr>
2021-03-10 16:19:48 +0100

    improvement footer css

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,17 +1,15 @@
-<template
-  ><v-container fluid pa-0>
-    <v-footer absolute color="light-blue darken-3"
-      ><v-col justify-space-between
-        ><span class="white--text"
-          >&copy;{{ new Date().getFullYear() }}
-        </span></v-col
-      ><v-col justify-space-between class="text-center"
-        ><strong class="white--text">@ Andres - Pascal</strong></v-col
+<template>
+  <v-card height="40px" rounded="0">
+    <v-footer padless fixed color="#026AA7"
+      ><v-col justify-space-between class="text-left footer white--text"
+        >&copy;{{ new Date().getFullYear() }} </v-col
+      ><v-col justify-space-between class="text-center footer white--text"
+        ><strong class="">@ Andres - Pascal</strong></v-col
       >
-      <v-col justify-space-between class="text-right"
-        ><span class="white--text ">Toulouse - FRANCE</span></v-col
+      <v-col justify-space-between class="text-right footer white--text"
+        >Toulouse - FRANCE</v-col
       >
-    </v-footer></v-container
+    </v-footer></v-card
   >
 </template>
 
